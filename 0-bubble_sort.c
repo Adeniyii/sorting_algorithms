aeef6fde4462b513f64fbd5f4f327d1dc3a06248
Adeniyii <ifedayoadeniyi@gmail.com>
2023-04-18 17:34:51 +0100

    feat: Implement Bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using the
+ * 						 Bubble sort algorithm.
+ *
+ * @array: The array to be sorted
+ * @size: The size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t swapped, i, tmp;
+
+	swapped = 1;
+
+	while (swapped)
+	{
+		swapped = 0;
+
+		for (i = 1; i < size; i++)
+		{
+			if (array[i] < array[i-1])
+			{
+				tmp = array[i];
+				array[i] = array[i-1];
+				array[i-1] = tmp;
+
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+	}
+}
