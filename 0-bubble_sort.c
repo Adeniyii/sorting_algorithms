2fa9ff797150f0934561f0e308d1d656c1160279
Adeniyii <ifedayoadeniyi@gmail.com>
2023-04-18 17:47:24 +0100

    fix: Only sort if array length >= 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,7 +2,7 @@
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order using the
- * 						 Bubble sort algorithm.
+ * Bubble sort algorithm.
  *
  * @array: The array to be sorted
  * @size: The size of the array
@@ -10,6 +10,9 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t swapped, i, tmp;
+
+	if (size < 2)
+		return;
 
 	swapped = 1;
 
@@ -19,11 +22,11 @@ void bubble_sort(int *array, size_t size)
 
 		for (i = 1; i < size; i++)
 		{
-			if (array[i] < array[i-1])
+			if (array[i] < array[i - 1])
 			{
 				tmp = array[i];
-				array[i] = array[i-1];
-				array[i-1] = tmp;
+				array[i] = array[i - 1];
+				array[i - 1] = tmp;
 
 				swapped = 1;
 				print_array(array, size);
