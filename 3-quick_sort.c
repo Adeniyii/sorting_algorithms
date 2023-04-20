07748a742968d3befe22addb5be4086989471959
Adeniyii <ifedayoadeniyi@gmail.com>
2023-04-20 12:55:31 +0100

    feat: Implement quick sort algo

---
+++ 3-quick_sort.c
@@ -0,0 +1,60 @@
+#include "sort.h"
+
+int partition(int *array, size_t size);
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using the
+ * Quick sort algorithm
+ *
+ * @array: array to sort
+ * @size: size of array
+*/
+void quick_sort(int *array, size_t size)
+{
+	int pivot;
+
+	if (size < 2)
+		return;
+
+	pivot = partition(array, size);
+	quick_sort(array, pivot);
+	quick_sort(array, size - pivot);
+}
+
+/**
+ * partition - partitions array around pivot
+ *
+ * @array: array to partition
+ * @size: size of array
+ *
+ * Return: index of pivot
+*/
+int partition(int *array, size_t size)
+{
+	size_t pivot, tmp, i, j;
+
+	pivot = size - 1;
+	j = -1;
+
+	for (i = 0; i < pivot; i++)
+	{
+		printf("arr[i]=%d, array[pivot]=%d\n", array[i], array[pivot]);
+
+		if (array[i] < array[pivot])
+		{
+			j++;
+			tmp = array[j];
+			array[j] = array[i];
+			array[i] = tmp;
+
+			print_array(array, size);
+		}
+	}
+	printf("out\n");
+	j++;
+	tmp = array[j];
+	array[j] = array[pivot];
+	array[pivot] = tmp;
+
+	return (j);
+}
